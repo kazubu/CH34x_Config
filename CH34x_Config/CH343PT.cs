68dc69af027e43c6cb57c7f249c236a2b1857012
Kazuki Shimizu <kazubu@jtime.net>
2024-06-27 03:13:38 +0900

    プロジェクト ファイルを追加します。

---
+++ CH34x_Config/CH343PT.cs
@@ -0,0 +1,88 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
+
+namespace CH34x_Config
+{
+    internal class CH343PT
+    {
+        public const string DLL_NAME = "CH343PT.DLL";
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ChipPropertyS
+        {
+            public byte ChipType;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string ChipTypeStr;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string FwVerStr;
+            public byte GpioCount;
+            [MarshalAs(UnmanagedType.Bool)]
+            public bool IsEmbbedEeprom;
+            [MarshalAs(UnmanagedType.Bool)]
+            public bool IsSupportMcuBootCtrl;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string ManufacturerString;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string ProductString;
+            public ushort bcdDevice;
+            public byte PortIndex;
+            public bool IsSupportGPIOInit;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string PortName;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+            public uint[] ResvD;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public class USERCFG_340
+        {
+            public byte SIG = 0x5b;
+            public byte MODE = 0x23;
+            public byte CFG = 0;
+            public byte WP = 0;
+            public ushort Vid = 0;
+            public ushort Pid = 0;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 2)]
+            public string Reserve1 = "";
+            public byte Power = 0;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 5)]
+            public string Reserve2 = "";
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 8)]
+            public string SN = "";
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 2)]
+            public string Reserve3 = "";
+            public byte PROD_LEN = 0;
+            public byte PROD_HDR_03H = 0x3;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 36)]
+            public string PROD = "";
+        }
+
+        [DllImport(DLL_NAME)]
+        [return: MarshalAs(UnmanagedType.U4)]
+        public static extern uint CH343PT_GetVersion();
+
+        [DllImport(DLL_NAME)]
+        public static extern bool CH343PT_NameIsCH34x(string iPortName);
+
+        [DllImport(DLL_NAME)]
+        public static extern bool CH343PT_HandleIsCH34x(SafeFileHandle iPortHandle);
+
+        [DllImport(DLL_NAME)]
+        public static extern bool CH343PT_EnterConfigMode(SafeFileHandle iPortHandle);
+
+        [DllImport(DLL_NAME)]
+        public static extern bool CH343PT_ExitConfigMode(SafeFileHandle iPortHandle);
+
+        [DllImport(DLL_NAME)]
+        public static extern byte CH343PT_GetChipProperty(SafeFileHandle iPortHandle, out ChipPropertyS chipPropertyS);
+    }
+}
---
+++ CH34x_Config/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CH34x_Config;
+using Microsoft.Win32.SafeHandles;
+using Newtonsoft.Json;
+
+string target = args.Length > 0 ? args[0] : "COM7";
+Console.WriteLine("Target port: " + target);
+
+uint ver = CH343PT.CH343PT_GetVersion();
+Console.WriteLine("DLL ver: " + ver/16 + "." + ver%16);
+
+// No need to use Name
+//bool isCH34x_Name = CH343PT.CH343PT_NameIsCH34x(target);
+//Console.WriteLine("isCH34X: " + isCH34x_Name);
+
+using (SafeFileHandle com_handle = File.OpenHandle(path: target, mode: FileMode.Open, access: FileAccess.ReadWrite))
+{
+    bool isCH34x_Handle = CH343PT.CH343PT_HandleIsCH34x(com_handle);
+    Console.WriteLine("isCH34X: " + isCH34x_Handle);
+    byte result = CH343PT.CH343PT_GetChipProperty(com_handle, out CH343PT.ChipPropertyS chipProperty);
+    Console.WriteLine("res:" + result);
+    Console.WriteLine("prop:" + JsonConvert.SerializeObject(chipProperty, Formatting.Indented));
+}
